e94aec44d426d651d4ff3d1c3a23316d97bf54e3
Anthea Ip <antheaip964@gmail.com>
2021-11-06 14:06:14 -0400

    Saving Frontend

---
+++ src/components/Main.js
@@ -0,0 +1,5 @@
+function Main(props){
+    return <h1>Main Name</h1>
+  } 
+  
+  export default Main
--- src/index.js
+++ src/index.js
@@ -1,5 +1,9 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+// Import SCSS file to be source of styling
+import "./styles.scss"
+// Import router
+import { BrowserRouter as Router } from "react-router-dom";
 import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
