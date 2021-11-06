1cd028ffbf1b4ae3fce71d97f9383a82e237a2fe
Anthea Ip <antheaip964@gmail.com>
2021-11-06 14:21:50 -0400

    First Main edit

--- src/components/Main.js
+++ src/components/Main.js
@@ -1,5 +1,29 @@
+import {useEffect, useState } from "react";
+import { Route, Switch } from "react-router-dom";
+import Index from "../pages/Index";
+import Show from "../pages/Show";
+
 function Main(props){
-    return <h1>Main Name</h1>
+
+    return (
+        <main>
+            <Switch>
+                <Route exact path="/">
+                    <Index />
+                </Route>
+                
+                <Route
+                path="/cheese/:id"
+                render={(rp) => (
+                <Show
+                {...rp}
+                />
+                )}
+                />
+            </Switch>
+        </main>
+    );
+    
   } 
   
   export default Main
